75b5d1469d536ad256c3f05b46a5afa1c2d44837
Evgeny Mandrikov <mandrikov@gmail.com>
2011-09-16 12:31:24 +0400

    Update example for try-with-resources

--- java7-language-features/src/main/java/TryWithResources.java
+++ java7-language-features/src/main/java/TryWithResources.java
@@ -1,13 +1,19 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 class TryWithResources {
 
   public void example() throws Exception {
+    // single resource
     try (FileInputStream in = new FileInputStream("foo.txt")) {
       int k;
       while ((k = in.read()) != -1) {
         System.out.write(k);
       }
+    }
+
+    // multiple resources
+    try (FileInputStream in = new FileInputStream("foo.txt"); FileOutputStream out = new FileOutputStream("bar.txt")) {
     }
   }
 
